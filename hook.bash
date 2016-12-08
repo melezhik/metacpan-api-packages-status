d3f57983cb402ea1f6f840e32f1348168586fbce
Alexey Melezhik <melezhik@gmail.com>
2016-12-08 12:16:11 +0300

    parse lines

--- hook.bash
+++ hook.bash
@@ -2,6 +2,6 @@ if [ -t 0 ]; then
     echo fetching 02packages.details.txt by http not implemented yet
 else
     while read -r line ; do
-        echo $line
+        run_story package-status line "'$line'"
     done
 fi
---
+++ modules/package-status/story.bash
@@ -0,0 +1,6 @@
+line=$(story_var line);
+echo $line
+
+#package=$(echo $line |perl -n -e '@s = split; print $s[0])
+#echo $package
+#curl https://fastapi.metacpan.org/v1/module/`
